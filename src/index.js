74e8e56cfe3868f15c08268066836b17c7bfdf65
Aleksey Dobrovolsky <a.dobrovolsky1991@gmail.com>
2019-02-20 21:00:05 +0300

    feat: add the feature of counting love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let firstLoves , secondLoves , thirdLoves, firstPosition,
+        secondPosition, thirdPosition, firstPositionArray,secondPositionArray, thirdPositionArray ;
+    let count = 0;
+    for (let i=0; i<preferences.length; i++){
+        firstLoves=secondLoves=thirdLoves=firstPosition =
+            secondPosition= thirdPosition= firstPositionArray=
+                secondPositionArray= thirdPositionArray=0;
+        firstPositionArray = i;
+        firstPosition = firstPositionArray+1;
+        firstLoves = secondPosition = preferences[firstPositionArray];
+        secondPositionArray = secondPosition-1;
+
+        if (secondPosition !== 0 && secondPosition <= preferences.length && firstPosition!==secondPosition) {
+            secondLoves = thirdPosition = preferences[secondPositionArray];
+            thirdPositionArray = thirdPosition-1;
+        } else continue;
+
+        if (secondPosition !== 0 && secondPosition <= preferences.length && secondPosition!==thirdPosition){
+            thirdLoves = preferences[thirdPositionArray];
+        } else continue;
+
+        if (thirdLoves === firstPosition ) count++;
+  }
+    return count/3;
 };
